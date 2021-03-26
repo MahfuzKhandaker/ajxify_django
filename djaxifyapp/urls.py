6ef5bf86f0868925f2d4fe3ef5ef78ec7f7ccc1f
Mahfuz Khandaker <mahfuzkhandaker.bd@gmail.com>
2021-03-27 02:59:33 +0600

    ajax paging

--- djaxifyapp/urls.py
+++ djaxifyapp/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from djaxifyapp.views import post_list, PostCreateView,  post_detail,  favourite_post, post_favourite_list, likes
+from djaxifyapp.views import PostListView, PostCreateView,  post_detail,  favourite_post, post_favourite_list, likes
 
 
 urlpatterns = [
-    path('', post_list, name='post-list'),
+    path('', PostListView.as_view(), name='post-list'),
     path('likes/', likes, name='likes'),
     path('favourites/', post_favourite_list, name='post_favourite_list'),
     path('<slug:slug>/', post_detail, name='post-detail'),
